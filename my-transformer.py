7a79dd03bb18480d2a63d80b79824dbf465c283f
kg <ksgarg@adobe.com>
2023-10-15 10:12:10 -0700

    fixed some bugs in transformer code

---
+++ my-transformer.py
@@ -0,0 +1,407 @@
+import ipdb
+import os
+from os.path import exists
+import torch
+import torch.nn as nn
+from torch.nn.functional import log_softmax, pad
+import math
+import copy
+import time
+from torch.optim.lr_scheduler import LambdaLR
+import pandas as pd
+import altair as alt
+from torchtext.data.functional import to_map_style_dataset
+from torch.utils.data import DataLoader
+from torchtext.vocab import build_vocab_from_iterator
+import torchtext.datasets as datasets
+import spacy
+import GPUtil
+import warnings
+from torch.utils.data.distributed import DistributedSampler
+import torch.distributed as dist
+import torch.multiprocessing as mp
+from torch.nn.parallel import DistributedDataParallel as DDP
+
+
+# Set to False to skip notebook execution (e.g. for debugging)
+warnings.filterwarnings("ignore")
+RUN_EXAMPLES = True
+# Some convenience helper functions used throughout the notebook
+
+
+def is_interactive_notebook():
+    return __name__ == "__main__"
+
+
+def show_example(fn, args=[]):
+    if __name__ == "__main__" and RUN_EXAMPLES:
+        return fn(*args)
+
+
+def execute_example(fn, args=[]):
+    if __name__ == "__main__" and RUN_EXAMPLES:
+        fn(*args)
+
+
+class DummyOptimizer(torch.optim.Optimizer):
+    def __init__(self):
+        self.param_groups = [{"lr": 0}]
+        None
+
+    def step(self):
+        None
+
+    def zero_grad(self, set_to_none=False):
+        None
+
+
+class DummyScheduler:
+    def step(self):
+        None
+
+
+
+
+class EncoderDecoder(nn.Module):
+    """
+    A standard encoder-decoder architecture.
+    """
+
+    def __init__(self, encoder, decoder, src_embed, tgt_embed, generator):
+        super(EncoderDecoder, self).__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+        self.src_embed = src_embed
+        self.tgt_embed = tgt_embed
+        self.generator = generator
+
+    def forward(self, src, tgt, src_mask, tgt_mask):
+        """take in and tgt and their respective masks"""
+        return self.decode(self.encode(src, src_mask), src_mask, tgt, tgt_mask)
+
+
+    def encode(self, src, src_mask):
+        return self.encoder(self.src_embed(src), src_mask)
+
+    def decode(self, memory, src_mask, tgt, tgt_mask):
+        return self.decoder(self.tgt_embed(tgt), memory, src_mask, tgt_mask)
+
+
+class Generator(nn.Module):
+    """Define standard linear + softmax"""
+
+    def __init__(self, d_model, vocab):
+        super(Generator, self).__init__()
+        self.proj = nn.Linear(d_model, vocab)
+
+    def forward(self, x):
+        return log_softmax(self.proj(x), dim=-1)
+
+
+
+def clones(module, N):
+    "Produce N identical deep copies"
+    return nn.ModuleList([copy.deepcopy(module) for _ in range(N)])
+
+
+class Encoder(nn.Module):
+    "Core encoder is a stack of N layers"
+
+    def __init__(self, layer, N):
+        super(Encoder, self).__init__()
+        self.layers = clones(layer,N)
+        self.norm = LayerNorm(layer.size)
+
+
+    def forward(self, x, mask):
+        """Pass the input (and mask) through each layer in turn"""
+        for layer in self.layers:
+            x = layer(x, mask)
+        return self.norm(x)
+
+
+class LayerNorm(nn.Module):
+    """Construct a layernorm"""
+
+    def __init__(self, features, eps =1e-6):
+        """Features here is number of channels/features"""
+        super(LayerNorm, self).__init__()
+        self.a_2 = nn.Parameter(torch.ones(features))  #gamma - scaling
+        self.b_2 = nn.Parameter(torch.zeros(features)) #offset
+        self.eps = eps
+
+    def forward(self, x):
+        mean = x.mean(-1, keepdim=True)
+        std = x.std(-1, keepdim=True)
+        return self.a_2 * (x-mean) / (std + self.eps) + self.b_2
+
+
+class SublayerConnection(nn.Module):
+    """
+    Connection residual connection (aka x+ LayerNorm((sublayer))
+    """
+
+    def __init__(self, size, dropout):
+        super(SublayerConnection, self).__init__()
+        self.norm = LayerNorm(size)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x, sublayer):
+        return x + self.dropout(sublayer(self.norm(x)))
+
+
+class EncoderLayer(nn.Module):
+    """Encoder is made of self_attention and feedforward connection"""
+
+    def __init__(self, size, self_attn, feed_forward, dropout):
+        super(EncoderLayer, self).__init__()
+        self.self_attn = self_attn
+        self.feed_forward = feed_forward
+        self.sublayer = clones(SublayerConnection(size,dropout),2) # make two sublayerconection one each for attn and feedforward
+        self.size = size # what is this size
+
+    def forward(self, x, mask):
+         x = self.sublayer[0](x,lambda x: self.self_attn(x,x,x, mask))
+         return self.sublayer[1](x, self.feed_forward)
+
+
+class Decoder(nn.Module):
+    """Generic N layer decoder"""
+
+    def __init__(self, layer, N):
+        super(Decoder, self).__init__()
+        self.layers = clones(layer, N)
+        self.norm = LayerNorm(layer.size) # what is a layer (decoder layer and decoder layer size not sure)
+
+    def forward(self, x, memory, src_mask, tgt_mask):
+        for layer in self.layers:
+            x = layer(x, memory, src_mask, tgt_mask)
+        return self.norm(x)
+
+
+
+class DecoderLayer(nn.Module):
+    """Decoder is made of self-att, src-attn and feedforward"""
+
+
+    def __init__(self, size, self_attn, src_attn, feed_forward, dropout):
+        """size here is number of channels/features in the sublayers"""
+        super(DecoderLayer, self).__init__()
+        self.size = size
+        self.self_attn = self_attn
+        self.src_attn = src_attn # is this a different function or just attent with different inputs
+        self.feed_forward = feed_forward
+        self.sublayer = clones(SublayerConnection(size, dropout),3) #3 layers
+
+    def forward(self, x, memory, src_mask, tgt_mask):
+        """Follow Fig 1 for connections"""
+
+        m = memory
+        x = self.sublayer[0](x, lambda x: self.self_attn(x, x, x, tgt_mask))
+        x = self.sublayer[1](x, lambda x: self.src_attn(x, m, m, src_mask))
+        return self.sublayer[2](x, self.feed_forward)
+
+
+def subsequent_mask(size):
+    """Mask out the subsequent positions. So only i and i-1, ... zero out i+1, i+2, ..."""
+    """ size is not the dimension by of the embeddings but size of input then, this size may differ from the layernorm size -- possibly"""
+
+    attn_shape = (1, size, size)
+    subsequent_mask = torch.triu(torch.ones(attn_shape), diagonal=1).type(torch.uint8)
+    return subsequent_mask == 0
+
+
+def example_mask():
+    LS_data = pd.concat(
+        [
+            pd.DataFrame(
+                {
+                    "Subsequent Mask": subsequent_mask(20)[0][x, y].flatten(),
+                    "Window": y,
+                    "Masking": x,
+                }
+            )
+            for y in range(20)
+            for x in range(20)
+        ]
+    )
+
+    return (
+        alt.Chart(LS_data)
+        .mark_rect()
+        .properties(height=250, width=250)
+        .encode(
+            alt.X("Window:O"),
+            alt.Y("Masking:O"),
+            alt.Color("Subsequent Mask:Q", scale=alt.Scale(scheme="viridis")),
+        )
+        .interactive()
+    )
+
+
+#show_example(example_mask)
+
+
+
+def attention(query, key, value, mask=None, dropout=None):
+    """Compute Scaled Dot Product Attention"""
+    """key could be batch_size, heads, seq_length, features"""
+
+    d_k = query.size(-1)
+    scores = torch.matmul(query,key.transpose(-2,-1)) / math.sqrt(d_k)
+    if mask is not None:
+        scores = scores.masked_fill(mask==0,-1e9)
+    p_attn = scores.softmax(dim=-1)
+    if dropout is not None:
+        p_attn = dropout(p_attn) # why dropout before matmul
+    return torch.matmul(p_attn, value), p_attn
+
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, h, d_model, dropout=0.1):
+        super(MultiHeadAttention, self).__init__()
+        assert d_model % h == 0
+        # we assume d_v always eq d_k
+        #d_v and d_k is derived from d_model less head may mean higher d_v
+        self.d_k = d_model // h
+        self.h = h
+        self.linears = clones(nn.Linear(d_model, d_model),4) # 4 matrices, K, Q, V, O
+        self.attn = None
+        self.dropout = nn.Dropout(p=dropout)
+
+
+    def forward(self, query, key, value, mask=None):
+        "Implement figure 2 - multihead figure"
+        if mask is not None:
+            mask = mask.unsqueeze(1)
+        nbatches = query.size(0)
+
+        # do all the linear projection in batch from d_model -> h * d_k
+        query, key, value = [
+            lin(x).view(nbatches, -1, self.h, self.d_k).transpose(1,2)
+            for lin, x in zip(self.linears, (query, key, value))
+            ]
+
+        # 2) apply attetnion on all the projected vectors in batch
+        x, self.attn = attention(query, key, value, mask=mask, dropout = self.dropout)
+
+        # 3 "Concat" using a view and apply a final lineear (not sure what this does)
+        x = (x.transpose(1,2).contiguous().view(nbatches, -1, self.h*self.d_k))
+        del query
+        del key
+        del value
+        return self.linears[-1](x)
+
+
+class PositionwiseFeedForward(nn.Module):
+    """Implements FNN equation"""
+
+
+    def __init__(self, d_model, d_ff, dropout = 0.1):
+        super(PositionwiseFeedForward, self).__init__()
+        self.w_1 = nn.Linear(d_model, d_ff)
+        self.w_2 = nn.Linear(d_ff, d_model)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        return self.w_2(self.dropout(self.w_1(x).relu()))
+
+
+class Embeddings(nn.Module):
+    def __init__(self, d_model, vocab):
+        super(Embeddings, self).__init__()
+        self.lut = nn.Embedding(vocab, d_model)
+        self.d_model = d_model
+
+    def forward(self, x):
+        return self.lut(x) * math.sqrt(self.d_model)
+
+
+class PositionalEncoding(nn.Module):
+    "Implement PE"
+
+
+    def __init__(self, d_model, dropout, max_len=5000):
+        super(PositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        # compute the positional encodings once in log space (why log space)
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len).unsqueeze(1)
+        div_term = torch.exp(
+            torch.arange(0, d_model, 2) * -(math.log(10000.0) / d_model))
+
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+
+        pe = pe.unsqueeze(0)
+        self.register_buffer("pe",pe)
+
+    def forward(self, x):
+        x = x + self.pe[:, : x.size(1)].requires_grad_(False)
+        return self.dropout(x)
+
+
+
+# Full Model
+
+def make_model( src_vocab, tgt_vocab, N=6, d_model=512, d_ff=2048, h=8, dropout=0.1):
+    """Helper: Construct a model from hyperparameters"""
+    c = copy.deepcopy
+    attn = MultiHeadAttention(h, d_model)
+    ff = PositionwiseFeedForward(d_model, d_ff, dropout)
+    position = PositionalEncoding(d_model, dropout)
+
+    model = EncoderDecoder(
+        Encoder(EncoderLayer(d_model, c(attn), c(ff), dropout),N),
+        Decoder(DecoderLayer(d_model, c(attn), c(attn), c(ff), dropout), N),
+        nn.Sequential(Embeddings(d_model, src_vocab), c(position)),
+        nn.Sequential(Embeddings(d_model, tgt_vocab), c(position)),
+        Generator(d_model, tgt_vocab)
+        )
+
+    #This was important for their code
+    for p in model.parameters():
+        if p.dim() > 1:
+            nn.init.xavier_uniform_(p)
+        return model
+
+
+# Inference
+
+def inference_test():
+    test_model = make_model(11, 11, 2)
+    test_model.eval()
+    src = torch.LongTensor([[1,2,3,4,5,6,7,8,9,10]])
+    src_mask = torch.ones(1,1,10)
+
+    memory = test_model.encode( src, src_mask)
+    ys = torch.zeros(1,1).type_as(src)
+
+    for i in range(9):
+        out = test_model.decode(memory, src_mask, ys, subsequent_mask(ys.size(1)).type_as(src.data))
+        prob = test_model.generator(out[:, -1])
+        _ , next_word = torch.max(prob, dim=1)
+        next_word = next_word.data[0]
+        ys = torch.cat([ys, torch.empty(1,1).type_as(src.data).fill_(next_word)], dim=1)
+
+    print("Example Untrained Model Precdiction:", ys)
+
+def run_tests():
+    for _ in range(10):
+        inference_test()
+
+
+show_example(run_tests)
+
+
+# Part 2 training
+
+# Batches and masking
+
+class Batch:
+    """Object for holding a batch of data"""
+
+    def __init__(self, src, tgt=None, pad=2):
+        self.src = src
+        self.src_mask = (src != pad).unsqueeze(-2
