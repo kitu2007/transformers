f094ad918f4dfbe838e9d76cfe6c5423c1913aa2
kg <ksgarg@adobe.com>
2023-10-23 14:07:17 -0700

    added example 2 (toy-example) and started with 3 (proper example)

--- my-transformer.py
+++ my-transformer.py
@@ -402,6 +402,295 @@ show_example(run_tests)
 class Batch:
     """Object for holding a batch of data"""
 
-    def __init__(self, src, tgt=None, pad=2):
+    def __init__(self, src, tgt=None, pad=2): #2 = blank
         self.src = src
-        self.src_mask = (src != pad).unsqueeze(-2
+        self.src_mask = (src != pad).unsqueeze(-2)
+        if tgt is not None:
+            self.tgt = tgt[:,:-1] # doesn't take the last (why)
+            self.tgt_y = tgt[:,1:] # deosn't take the first so prediction second word based on first..  
+            self.tgt_mask = self.make_std_mask(self.tgt, pad)
+            self.ntokens = (self.tgt_y != pad).data.sum()
+
+
+    @staticmethod
+    def make_std_mask(tgt, pad):
+        "create a mask to hide padding and future words"
+
+        tgt_mask = (tgt != pad).unsqueeze(-2)
+        tgt_mask = tgt_mask & subsequent_mask(tgt.size(-1)).type_as(tgt_mask.data))
+        return tgt_mask
+
+
+
+# Training loop
+class TrainState:
+    """Track nmber of steps, examples, and tokens processed"""
+    step: int = 0
+    accum_step: int = 0 # number of gradient acc steps
+    samples: int = 0
+    tokens: int = 0
+
+ def run_epoch(data_iter, model, loss_compute, optimizer, scheduler, mode="train", accum_iter=1, train_state=TrainState(),):
+     """Train a single epich"""
+
+     start = time.time()
+     total_tokens = 0
+     total_loss = 0
+     tokens = 0
+     n_accum = 0
+     for i, batch in enumerate(data_iter):
+         out = model.forward(batch.src, batch.tgt, batch.src_mask, batch.tgt_mask)
+         loss, loss_node = loss_compute(out, batch.tgt_y, batch.ntokens)
+         # loss_node = loss_node / accum_iter
+         if mode == "train" or mode == "train+log":
+             loss_node.backward()
+             train_state.step += 1
+             train_state.samples += batch.src.shape[0]
+             train_state.tokens += batch.ntokens
+             if i % accum_iter == 0:
+                 optimizer.step()
+                 optimizer.zero_grad(set_to_none=True)
+                 n_accum += 1
+                 train_state.accum_step += 1
+             scheduler.step()
+
+         total_loss += loss
+         total_tokens += batch.ntokens
+         tokens += batch.ntokens
+
+         if i % 40 == 1 and (mode == "train" or mode == "train+log"):
+             lr = optimizer.param_groups[0]["lr"]
+             elapsed = time.time() - start
+             print(
+                 (
+                    "Epoch Step: %6d | Accumulation Step: %3d | Loss: %6.2f "
+                    + "| Tokens / Sec: %7.1f | Learning Rate: %6.1e"
+                  )
+                 % (i, n_accum, loss / batch.ntokens, tokens / elapsed, lr)
+                 )
+             start = time.time()
+             tokens = 0
+             del loss
+             del loss_node
+
+        
+   return total_loss / total_tokens, train_state
+
+
+def rate(step, model_size, factor, warmup):
+    """
+    we have to default the step to 1 from LambdaR function
+    to avoid zero raising to negative power
+    """
+    if step == 0:
+        step = 1
+    return factor * (model_size ** (-0.5) * min(step ** (-0.5), step * warmup ** (-1.5)))
+
+def example_learning_schedule():
+    opts = [
+        [512, 1, 4000], #example 1
+        [512, 1, 8000], #example 1
+        [256, 1, 4000], #example 1
+        ]
+
+    dummy_model = torch.nn.Linear(1, 1)
+    learning_rates = []
+
+    # we have 3 examples in opts list.
+    for idx, example in enumerate(opts):
+        # run 20000 epoch for each example
+        optimizer = torch.optim.Adam(
+            dummy_model.parameters(), lr=1, betas=(0.9, 0.98), eps=1e-9
+        )
+        lr_scheduler = LambdaLR(
+            optimizer=optimizer, lr_lambda=lambda step: rate(step, *example)
+        )
+        tmp = []
+        # take 20K dummy training steps, save the learning rate at each step
+        for step in range(20000):
+            tmp.append(optimizer.param_groups[0]["lr"])
+            optimizer.step()
+            lr_scheduler.step()
+        learning_rates.append(tmp)
+
+    learning_rates = torch.tensor(learning_rates)
+
+    # Enable altair to handle more than 5000 rows
+    alt.data_transformers.disable_max_rows()
+
+    opts_data = pd.concat(
+        [
+            pd.DataFrame(
+                {
+                    "Learning Rate": learning_rates[warmup_idx, :],
+                    "model_size:warmup": ["512:4000", "512:8000", "256:4000"][
+                        warmup_idx
+                    ],
+                    "step": range(20000),
+                }
+            )
+            for warmup_idx in [0, 1, 2]
+        ]
+    )
+
+    return (
+        alt.Chart(opts_data)
+        .mark_line()
+        .properties(width=600)
+        .encode(x="step", y="Learning Rate", color="model_size:warmup:N")
+        .interactive()
+    )
+
+
+# example_learning_schedule()
+
+
+def loss(x, crit):
+    d = x + 3 * 1
+    predict = torch.FloatTenssor([[0, x/d, 1/d, 1,d]])
+    return crit(predit.log(), torch.LongTensor([1])).data
+
+
+def penalization_visualization():
+    crit = LabelSmoothing(5, 0, 0.1)
+    loss_data = pd.DataFrame(
+        {
+            "Loss": [loss(x, crit) for x in range(1, 100)],
+            "Steps": list(range(99)),
+        }
+    ).astype("float")
+
+    return (
+        alt.Chart(loss_data)
+        .mark_line()
+        .properties(width=350)
+        .encode(
+            x="Steps",
+            y="Loss",
+        )
+        .interactive()
+    )
+
+
+#show_example(penalization_visualization)
+
+# Synthetic Data
+
+def data_gen(V, batch_size, nbatches):
+    "Generate random data for a src-tgt copy task"
+    for i in range(nbatches):
+        data = torch.randint(1, V, size=(batch_size, 10))
+        data[:, 0] = 1
+        src = data.requires_grad_(False).clone().detach()
+        tgt = data.requires_grad_(False).clone().detach()
+        yield Batch(src, tgt, 0)
+
+
+# Loss Computation
+
+
+class SimpleLossCompute:
+    "A simple loss compute and train function."
+
+    def __init__(self, generator, criterion):
+        self.generator = generator
+        self.criterion = criterion
+
+    def __call__(self, x, y, norm):
+        x = self.generator(x)
+        sloss = (
+            self.criterion(
+                x.contiguous().view(-1, x.size(-1)), y.contiguous().view(-1)
+            )
+            / norm
+        )
+        return sloss.data * norm, sloss
+
+
+# Greedy Decoding
+
+def greedy_decode(model, src, src_mask, max_len, start_symbol):
+    memory = model.encode(src, src_mask)
+    ys = torch.zeros(1, 1).fill_(start_symbol).type_as(src.data)
+    for i in range(max_len - 1):
+        out = model.decode(
+            memory, src_mask, ys, subsequent_mask(ys.size(1)).type_as(src.data)
+        )
+        prob = model.generator(out[:, -1])
+        _, next_word = torch.max(prob, dim=1)
+        next_word = next_word.data[0]
+        ys = torch.cat(
+            [ys, torch.zeros(1, 1).type_as(src.data).fill_(next_word)], dim=1
+        )
+    return ys
+
+
+# Train the simple copy task
+
+def example_simple_model():
+    V = 11
+    criterion = LabelSmoothing(size=V, padding_idx=0, smoothing=0.0)
+    model = make_model(V, V, N=2)
+
+    optimizer = torch.optim.Adam(
+        model.parameters(), lr=0.5, betas=(0.9, 0.98), eps=1e-9
+    )
+    lr_scheduler = LambdaLR(
+        optimizer=optimizer,
+        lr_lambda=lambda step: rate(
+            step, model_size=model.src_embed[0].d_model, factor=1.0, warmup=400
+        ),
+    )
+
+    batch_size = 80
+    for epoch in range(20):
+        model.train()
+        run_epoch(
+            data_gen(V, batch_size, 20),
+            model,
+            SimpleLossCompute(model.generator, criterion),
+            optimizer,
+            lr_scheduler,
+            mode="train",
+        )
+        model.eval()
+        run_epoch(
+            data_gen(V, batch_size, 5),
+            model,
+            SimpleLossCompute(model.generator, criterion),
+            DummyOptimizer(),
+            DummyScheduler(),
+            mode="eval",
+        )[0]
+
+    model.eval()
+    src = torch.LongTensor([[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]])
+    max_len = src.shape[1]
+    src_mask = torch.ones(1, 1, max_len)
+    print(greedy_decode(model, src, src_mask, max_len=max_len, start_symbol=0))
+
+
+# execute_example(example_simple_model)
+
+# Part 3: Real workd example
+
+# Load spacy tokenizer models, download them if they haven't been
+# downloaded already
+
+
+def load_tokenizers():
+
+    try:
+        spacy_de = spacy.load("de_core_news_sm")
+    except IOError:
+        os.system("python -m spacy download de_core_news_sm")
+        spacy_de = spacy.load("de_core_news_sm")
+
+    try:
+        spacy_en = spacy.load("en_core_web_sm")
+    except IOError:
+        os.system("python -m spacy download en_core_web_sm")
+        spacy_en = spacy.load("en_core_web_sm")
+
+    return spacy_de, spacy_en
+    
