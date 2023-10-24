7ef62096feb4c6775de5b1e2b4a4a22d2d4cf4b0
kitu2007 <kgarg191@gmail.com>
2023-10-24 03:10:48 +0000

    fx some simple debugging issues

--- my-transformer.py
+++ my-transformer.py
@@ -1,4 +1,4 @@
-import ipdb
+import pdb
 import os
 from os.path import exists
 import torch
@@ -417,7 +417,7 @@ class Batch:
         "create a mask to hide padding and future words"
 
         tgt_mask = (tgt != pad).unsqueeze(-2)
-        tgt_mask = tgt_mask & subsequent_mask(tgt.size(-1)).type_as(tgt_mask.data))
+        tgt_mask = tgt_mask & subsequent_mask(tgt.size(-1)).type_as(tgt_mask.data)
         return tgt_mask
 
 
@@ -430,51 +430,47 @@ class TrainState:
     samples: int = 0
     tokens: int = 0
 
- def run_epoch(data_iter, model, loss_compute, optimizer, scheduler, mode="train", accum_iter=1, train_state=TrainState(),):
-     """Train a single epich"""
+def run_epoch(data_iter, model, loss_compute, optimizer, scheduler, mode="train", accum_iter=1, train_state=TrainState()):
+    """Train a single epich"""
+    start = time.time()
+    total_tokens = 0
+    total_loss = 0
+    tokens = 0
+    n_accum = 0
+    for i, batch in enumerate(data_iter):
+        out = model.forward(batch.src, batch.tgt, batch.src_mask, batch.tgt_mask)
+        loss, loss_node = loss_compute(out, batch.tgt_y, batch.ntokens)
+        # loss_node = loss_node / accum_iter
+        if mode == "train" or mode == "train+log":
+            loss_node.backward()
+            train_state.step += 1
+            train_state.samples += batch.src.shape[0]
+            train_state.tokens += batch.ntokens
+            if i % accum_iter == 0:
+                optimizer.step()
+                optimizer.zero_grad(set_to_none=True)
+                n_accum += 1
+                train_state.accum_step += 1
+            scheduler.step()
 
-     start = time.time()
-     total_tokens = 0
-     total_loss = 0
-     tokens = 0
-     n_accum = 0
-     for i, batch in enumerate(data_iter):
-         out = model.forward(batch.src, batch.tgt, batch.src_mask, batch.tgt_mask)
-         loss, loss_node = loss_compute(out, batch.tgt_y, batch.ntokens)
-         # loss_node = loss_node / accum_iter
-         if mode == "train" or mode == "train+log":
-             loss_node.backward()
-             train_state.step += 1
-             train_state.samples += batch.src.shape[0]
-             train_state.tokens += batch.ntokens
-             if i % accum_iter == 0:
-                 optimizer.step()
-                 optimizer.zero_grad(set_to_none=True)
-                 n_accum += 1
-                 train_state.accum_step += 1
-             scheduler.step()
-
-         total_loss += loss
-         total_tokens += batch.ntokens
-         tokens += batch.ntokens
-
-         if i % 40 == 1 and (mode == "train" or mode == "train+log"):
-             lr = optimizer.param_groups[0]["lr"]
-             elapsed = time.time() - start
-             print(
-                 (
-                    "Epoch Step: %6d | Accumulation Step: %3d | Loss: %6.2f "
-                    + "| Tokens / Sec: %7.1f | Learning Rate: %6.1e"
+        total_loss += loss
+        total_tokens += batch.ntokens
+        tokens += batch.ntokens
+        if i % 40 == 1 and (mode == "train" or mode == "train+log"):
+            lr = optimizer.param_groups[0]["lr"]
+            elapsed = time.time() - start
+            print(
+                (
+                 "Epoch Step: %6d | Accumulation Step: %3d | Loss: %6.2f "
+                 + "| Tokens / Sec: %7.1f | Learning Rate: %6.1e"
                   )
-                 % (i, n_accum, loss / batch.ntokens, tokens / elapsed, lr)
-                 )
-             start = time.time()
-             tokens = 0
-             del loss
-             del loss_node
-
-        
-   return total_loss / total_tokens, train_state
+                % (i, n_accum, loss / batch.ntokens, tokens / elapsed, lr)
+                )
+            start = time.time()
+            tokens = 0
+        del loss
+        del loss_node
+    return total_loss / total_tokens, train_state
 
 
 def rate(step, model_size, factor, warmup):
@@ -544,6 +540,77 @@ def example_learning_schedule():
 
 # example_learning_schedule()
 
+class LabelSmoothing(nn.Module):
+    "Implement label smoothing."
+
+    def __init__(self, size, padding_idx, smoothing=0.0):
+        super(LabelSmoothing, self).__init__()
+        self.criterion = nn.KLDivLoss(reduction="sum")
+        self.padding_idx = padding_idx
+        self.confidence = 1.0 - smoothing
+        self.smoothing = smoothing
+        self.size = size
+        self.true_dist = None
+
+    def forward(self, x, target):
+        assert x.size(1) == self.size
+        true_dist = x.data.clone()
+        true_dist.fill_(self.smoothing / (self.size - 2))
+        true_dist.scatter_(1, target.data.unsqueeze(1), self.confidence)
+        true_dist[:, self.padding_idx] = 0
+        mask = torch.nonzero(target.data == self.padding_idx)
+        if mask.dim() > 0:
+            true_dist.index_fill_(0, mask.squeeze(), 0.0)
+        self.true_dist = true_dist
+        return self.criterion(x, true_dist.clone().detach())
+
+
+# Example of label smoothing.
+
+
+def example_label_smoothing():
+    crit = LabelSmoothing(5, 0, 0.4)
+    predict = torch.FloatTensor(
+        [
+            [0, 0.2, 0.7, 0.1, 0],
+            [0, 0.2, 0.7, 0.1, 0],
+            [0, 0.2, 0.7, 0.1, 0],
+            [0, 0.2, 0.7, 0.1, 0],
+            [0, 0.2, 0.7, 0.1, 0],
+        ]
+    )
+    crit(x=predict.log(), target=torch.LongTensor([2, 1, 0, 3, 3]))
+    LS_data = pd.concat(
+        [
+            pd.DataFrame(
+                {
+                    "target distribution": crit.true_dist[x, y].flatten(),
+                    "columns": y,
+                    "rows": x,
+                }
+            )
+            for y in range(5)
+            for x in range(5)
+        ]
+    )
+
+    return (
+        alt.Chart(LS_data)
+        .mark_rect(color="Blue", opacity=1)
+        .properties(height=200, width=200)
+        .encode(
+            alt.X("columns:O", title=None),
+            alt.Y("rows:O", title=None),
+            alt.Color(
+                "target distribution:Q", scale=alt.Scale(scheme="viridis")
+            ),
+        )
+        .interactive()
+    )
+
+
+#show_example(example_label_smoothing)
+
 
 def loss(x, crit):
     d = x + 3 * 1
@@ -587,7 +654,6 @@ def data_gen(V, batch_size, nbatches):
 
 
 # Loss Computation
-
 
 class SimpleLossCompute:
     "A simple loss compute and train function."
@@ -643,6 +709,8 @@ def example_simple_model():
     )
 
     batch_size = 80
+    pdb.set_trace()
+    xx = data_gen(V, batch_size, 20)
     for epoch in range(20):
         model.train()
         run_epoch(
@@ -670,27 +738,5 @@ def example_simple_model():
     print(greedy_decode(model, src, src_mask, max_len=max_len, start_symbol=0))
 
 
-# execute_example(example_simple_model)
-
-# Part 3: Real workd example
-
-# Load spacy tokenizer models, download them if they haven't been
-# downloaded already
-
-
-def load_tokenizers():
-
-    try:
-        spacy_de = spacy.load("de_core_news_sm")
-    except IOError:
-        os.system("python -m spacy download de_core_news_sm")
-        spacy_de = spacy.load("de_core_news_sm")
-
-    try:
-        spacy_en = spacy.load("en_core_web_sm")
-    except IOError:
-        os.system("python -m spacy download en_core_web_sm")
-        spacy_en = spacy.load("en_core_web_sm")
-
-    return spacy_de, spacy_en
+execute_example(example_simple_model)
     
